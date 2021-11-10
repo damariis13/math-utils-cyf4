d45380acf42f7a98db8c5d421f7d2be2ac9799dd
Damaris Dall'Orto <damarisdallorto@outlook.com>
2021-11-09 20:12:50 -0500

    operaciones aritmeticas

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const suma = (a,b) => {
+    return a+b;
+}
+
+suma(1,2);
+
+const resta = (a,b) => {
+    return a-b;
+}
+
+const multiplicacion = (a,b) => {
+    return a*b;
+} 
+
+const division = (a,b) => {
+    return a/b;
+}
+
